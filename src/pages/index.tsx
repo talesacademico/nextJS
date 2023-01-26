ab08d7194146ff007ed6bccbad9e51dab9dde97b
Tales Félix <tales.felix.10@gmail.com>
2023-01-26 11:31:57 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ignews</title>
+      </Head>
+      <h1>ok</h1>
+    </>
+  )
+}
